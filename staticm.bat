1d65ac1cb8f5e42c876388f33c9cdd2609ae4e1a
oleghbond <o.o.bondarenko@gmail.com>
2016-12-27 09:42:21 +0200

    Дрібні правки

--- staticm.bat
+++ staticm.bat
@@ -1,14 +1,15 @@
-if exist static\ del /s/q static\ > nul
-if exist index.html del /s/q index.html > nul
+if exist static\     del /s/q static\*.* > nul
+if exist static\     rd static\ > nul
+if exist index.html  del /s/q index.html > nul
 if exist static.html del /s/q static.html > nul
-if exist \temp1029384756 del /s/q \temp1029384756 > nul
-if exist \temp1029384756 rd \temp1029384756 > nul
+
+if exist wiki\output\static\ del /s/q wiki\output\static\*.* > nul
+if exist wiki\output\static\ rd wiki\output\static\ > nul
+if exist wiki\output         del /s/q wiki\output\*.* > nul
+if exist wiki\output         rd wiki\output > nul
 
 call tiddlywiki ./wiki --build
 
-md \temp1029384756
-xcopy /s/y wiki\output\*.* \temp1029384756 > nul
-xcopy /s/y \temp1029384756\*.*  > nul
-del /s/q \temp1029384756  > nul
-rd \temp1029384756 > nul
-del /s/q wiki\output > nul
+move wiki\output\static static > nul
+move wiki\output\*.* . > nul
+rd   wiki\output\ > nul
